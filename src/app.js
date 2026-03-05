2c1fd0219a11ad870b48a0b0be4fbfd4f1c426af
Harshit Garg <gargh2120@gmail.com>
2026-03-05 23:04:54 +0530

    adding functionality

--- src/app.js
+++ src/app.js
@@ -7,10 +7,18 @@ import healthCheck from "./routes/healthCheck.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import courseRouter from "./routes/course.routes.js";
 
+import {
+  verifyPayment,
+} from "./controllers/razorpay.controller.js"
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.post(
+  "/stripe/webhook",
+  express.raw({ type: "application/json" }),
+  verifyPayment
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookiesParse());
