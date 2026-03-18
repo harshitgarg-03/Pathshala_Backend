64c315c7bc17db3c895105d2ee7e8003d9c887d1
Harshit Garg <gargh2120@gmail.com>
2026-03-18 23:58:26 +0530

    vercel file

--- src/server.js
+++ src/server.js
@@ -6,11 +6,11 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log("Server start at: " + process.env.PORT);
-    });
+    
   })
   .catch((err) => {
     console.error("MongoDB connection error: ", err);
     process.exit(1);
   });
+
+export default app;
