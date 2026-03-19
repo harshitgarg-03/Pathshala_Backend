70dfe76c27eebd68415c9c432f0abc4dd3e7171a
Harshit Garg <gargh2120@gmail.com>
2026-03-20 00:55:49 +0530

    fix path

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import "dotenv/config";
 dotenv.config();
-import app from "./app.js";
-import { connectDB } from "./config/db.js";
+import app from "../src/app.js";
+import { connectDB } from "../src/config/db.js";
 connectDB()
   .then(() => {
     
