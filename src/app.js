344e13f6bc7e0416b3566c10233f7b6c644e714a
Harshit Garg <gargh2120@gmail.com>
2026-03-20 22:36:24 +0530

    remove limit

--- src/app.js
+++ src/app.js
@@ -23,8 +23,8 @@ app.post(
   express.raw({ type: "application/json" }),
   verifyPayment,
 );
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, }));
 app.use(cookiesParse());
 
 // cors confifrations
