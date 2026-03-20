cc41cb8df40c03c4416ed7db8df5c0213b84b1e0
Harshit Garg <gargh2120@gmail.com>
2026-03-20 20:37:23 +0530

    Fix cors issues

--- src/app.js
+++ src/app.js
@@ -29,13 +29,11 @@ app.use(cookiesParse());
 // cors confifrations
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || process.env.CORS_ORIGIN.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: [
+      process.env.CORS_ORIGIN,
+      "http://localhost:5173",
+      "http://localhost:4173",
+    ],
     credentials: true,
   }),
 );
